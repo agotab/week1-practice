a8ca77d389a330731da446c51a708560d727292c
agotab <agota0201@gmail.com>
2023-09-15 20:44:56 +0200

    basic layout and js practice

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log("I am Agota");
+//setInterval(()=>console.log("Hello"),1000);
+
+let x = 10;
+let y = 5;
+//console.log(x+y);
+x+y;
+let z = "Csésze"
+console.log(z)
+let z = "bögre"
+console.log(z)
